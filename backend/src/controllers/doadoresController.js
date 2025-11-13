21b1c340b629a90e9e7e71b4b7c2fd8081dabb99
Christian <christianbutkevicis@gmail.com>
2025-11-13 17:31:01 -0300

    feat(backend): adiciona rota PUT e DELETE na rota doadores

--- backend/src/controllers/doadoresController.js
+++ backend/src/controllers/doadoresController.js
@@ -41,7 +41,56 @@ const createDoador = async (req, res) => {
 
 }
 
+const updateDoador = async (req, res) => {
+    const { id } = req.params
+    const { nome, tipo_pessoa, documento, telefone, email } = req.body
+
+    try {
+        const query = `
+        UPDATE doadores
+        SET nome = ?, tipo_pessoa = ?, documento = ?, telefone = ?, email = ?
+        WHERE id_doador = ?
+        `
+
+        const [result] = await pool.query(query, [nome, tipo_pessoa, documento, telefone, email, id])
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Doador não encontrado ou nenhum dado alterado.' })
+        }
+        res.status(200).json({ message: 'Doador atualizado com sucesso!' })
+    } catch (error) {
+        console.error(error)
+        if (error.code === 'ER_DUP_ENTRY') {
+            return res.status(400).json({ message: 'Este documento (CPF/CNPJ) já está cadastrado em outro doador' })
+        }
+        res.status(500).json({ message: 'Erro ao atualizar doador no servidor.' })
+    }
+}
+
+const deleteDoador = async (req, res) => {
+    const { id } = req.params
+    try {
+        const query = 'DELETE FROM doadores WHERE id_doador = ?'
+        const [result] = await pool.query(query, [id])
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Doador não encontrado' })
+        }
+
+        res.status(200).json({ message: 'Doador removido com sucesso!' })
+    } catch (error) {
+        console.error(error)
+        if (error.code === 'ER_ROW_IS_REFERENCED_') {
+            return res.status(409).json({ message: 'Não é possivel remover. este doador possui doações registradas' })
+        }
+        res.status(500).json({ message: 'Erro ao remover doador no servidor.' })
+    }
+}
+
+
 module.exports = {
     getAllDoadores,
-    createDoador
+    createDoador,
+    updateDoador,
+    deleteDoador
 }
