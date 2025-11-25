1ba114d5b14e1d8fb22c27bcfbcdec618694f318
Christian <christianbutkevicis@gmail.com>
2025-11-24 21:08:07 -0300

    feat(backend): Implementação de login e cadastro no backend

--- backend/src/database/scheama.sql
+++ backend/src/database/scheama.sql
@@ -6,3 +6,19 @@ CREATE TABLE doadores (
     telefone VARCHAR(20)
     email VARCHAR(45)
 )
+
+CREATE TABLE familias (
+id_familia INT AUTO_INCREMENT PRIMARY KEY,
+nome_responsavel VARCHAR(255) NOT NULL, 
+endereco TEXT,
+contato VARCHAR(50)
+n_integrantes INT
+)
+
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
